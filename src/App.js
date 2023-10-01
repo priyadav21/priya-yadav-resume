7e884e1a2fc53235310d0bc463766c308236451c
Priya Yadav <priyadav218@gmail.com>
2023-10-01 14:58:45 +0530

    portfolio's initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import linkedInLogo from "./linkedIn.png";
+import profileImage from "./profileImage.jpeg";
+import githubLogo from "./github.svg";
+import resumeLogo from "./resume-icon.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={profileImage} className="App-logo" alt="logo" />
+        <h1>Priya Yadav</h1>
+        <div>
+          <a
+            className="link linkedIn"
+            href="https://www.linkedin.com/in/priyadav21/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedInLogo} className="social-logo" />
+          </a>
+          <a
+            className="link github"
+            href="https://github.com/priyadav21"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githubLogo} className="social-logo" />
+          </a>
+          <a
+            className="link resume"
+            href="https://drive.google.com/file/d/1zFxe99RYn0l17wT4qiq83tUWZGaZjKl5/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={resumeLogo} className="social-logo" />
+          </a>
+        </div>
       </header>
     </div>
   );
